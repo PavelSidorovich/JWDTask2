eeb958b56712dd401221cae600825ef25aee4cc7
Pavel Sidorovich <pavsid2001@mail.ru>
2021-07-31 19:27:52 +0300

    refactor: edit classes

--- src/main/java/com/epam/jwd/figures/model/point/Point.java
+++ src/main/java/com/epam/jwd/figures/model/point/Point.java
@@ -25,6 +25,14 @@ public class Point implements Figure {
     }
 
     @Override
+    public String toString() {
+        return "Point{" +
+               "x=" + x +
+               ", y=" + y +
+               '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) {
             return true;
