f4340f86b3358c565ac413eae40cd0acf66af9d0
Pavel Sidorovich <pavsid2001@mail.ru>
2021-08-04 18:14:11 +0300

    feat: add scalar multiply of vectors

--- src/main/java/com/epam/jwd/figures/utils/action/MathVector.java
+++ src/main/java/com/epam/jwd/figures/utils/action/MathVector.java
@@ -14,4 +14,8 @@ public class MathVector {
     public Double multiply(MathVector secondVector) {
         return this.x * secondVector.y - secondVector.x * this.y;
     }
+
+    public boolean perpendicular(MathVector secondVector) {
+        return Double.valueOf(0).equals(Math.abs(this.x * secondVector.x + this.y * secondVector.y));
+    }
 }
