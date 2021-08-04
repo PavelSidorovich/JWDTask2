918f1caea439495a26a48d6cc7baf87ccb9d2121
Pavel Sidorovich <pavsid2001@mail.ru>
2021-08-04 18:52:27 +0300

    refactor: enhance performance of convex define algorithm

--- src/main/java/com/epam/jwd/figures/utils/action/FigureActions.java
+++ src/main/java/com/epam/jwd/figures/utils/action/FigureActions.java
@@ -54,25 +54,26 @@ public class FigureActions {
     }
 
     public Boolean isConvex() {
+        boolean convex = true;
+        LinkedList<MathVector> vectors = figure.getVectors();
+        boolean negative = vectors.get(0).multiply(vectors.get(1)) < 0;
+
         if (figure instanceof Point) {
-            return false;
+            return true;
         }
-        boolean isConvex = true;
-        LinkedList<Point> points = figure.getPoints();
-        double T = points.get(figure.getNumberOfPoints() - 1).getX() * points.get(0).getY()
-                   - points.get(0).getX() * points.get(figure.getNumberOfPoints() - 1).getY();
-        double Z = T / Math.abs(T);
-        double P = 1;
-        for (int i = 0; i < points.size() - 2; i++) {
-            MathVector mathVector1 = new MathVector(points.get(i), points.get(i + 1));
-            MathVector mathVector2 = new MathVector(points.get(i + 1), points.get(i + 2));
-            P *= Z * mathVector1.multiply(mathVector2) / Math.abs(mathVector1.multiply(mathVector2));
-            if (P < 0) {
-                isConvex = false;
+        for (int i = 0; i < vectors.size(); i++) {
+            boolean tmp;
+            if (i != vectors.size() - 1) {
+                tmp = (vectors.get(i).multiply(vectors.get(i + 1)) < 0) != negative;
+            } else {
+                tmp = (vectors.get(i).multiply(vectors.get(0)) < 0) != negative;
+            }
+            if (tmp) {
+                convex = false;
                 break;
             }
         }
-        return isConvex;
+        return convex;
     }
 
     public Figure getFigure() {
