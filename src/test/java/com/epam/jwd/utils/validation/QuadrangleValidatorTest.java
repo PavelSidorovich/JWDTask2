0b3042b7700cfee125707f1b3fbba9734675339a
Pavel Sidorovich <pavsid2001@mail.ru>
2021-08-11 00:41:03 +0300

    add tests for QuadrangleValidator class

---
+++ src/test/java/com/epam/jwd/utils/validation/QuadrangleValidatorTest.java
@@ -0,0 +1,56 @@
+package com.epam.jwd.utils.validation;
+
+import com.epam.jwd.model.quadrangle.Point;
+import com.epam.jwd.model.quadrangle.PointFabric;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.LinkedList;
+
+import static org.testng.Assert.*;
+
+public class QuadrangleValidatorTest {
+
+    private static final PointFabric POINT_FABRIC = new PointFabric();
+    private static LinkedList<Point> points = new LinkedList<>();
+    private static QuadrangleValidator validator = null;
+
+    @AfterMethod
+    public void clearList() {
+        points.clear();
+    }
+
+    @Test
+    public void canBeBuild_shouldReturnFalse_whenNumberOfPointsIsInvalid() {
+        points.add(POINT_FABRIC.newInstance(0, 4));
+        points.add(POINT_FABRIC.newInstance(3, 4));
+        points.add(POINT_FABRIC.newInstance(5, 9));
+        points.add(POINT_FABRIC.newInstance(6, 1));
+        points.add(POINT_FABRIC.newInstance(8, 4));
+        validator = new QuadrangleValidator(points);
+
+        assertTrue(!validator.canBeBuild());
+    }
+
+    @Test
+    public void canBeBuild_shouldReturnTrue_whenPointsAreValid() {
+        points.add(POINT_FABRIC.newInstance(0, 4));
+        points.add(POINT_FABRIC.newInstance(3, 4));
+        points.add(POINT_FABRIC.newInstance(5, 9));
+        points.add(POINT_FABRIC.newInstance(6, 1));
+        validator = new QuadrangleValidator(points);
+
+        assertTrue(validator.canBeBuild());
+    }
+
+    @Test
+    public void canBeBuild_shouldReturnFalse_whenPointsOnSameLine() {
+        points.add(POINT_FABRIC.newInstance(1, 0));
+        points.add(POINT_FABRIC.newInstance(2, 0));
+        points.add(POINT_FABRIC.newInstance(3, 0));
+        points.add(POINT_FABRIC.newInstance(4, 0));
+        validator = new QuadrangleValidator(points);
+
+        assertFalse(validator.canBeBuild());
+    }
+}
