156e0560e8f280c839825156da9ed32686f367df
Pavel Sidorovich <pavsid2001@mail.ru>
2021-08-11 23:34:53 +0300

    optimized getVectors() method

--- src/main/java/com/epam/jwd/quadrangle/model/Figure.java
+++ src/main/java/com/epam/jwd/quadrangle/model/Figure.java
@@ -17,15 +17,12 @@ public interface Figure {
     default ArrayList<MathVector> getVectors() {
         ArrayList<MathVector> vectors = new ArrayList<>();
 
-        for (int i = 0; i < this.getNumberOfPoints(); i++) {
-            if (i != this.getNumberOfPoints() - 1) {
+        for (int i = 0; i < this.getNumberOfPoints() - 1; i++) {
                 vectors.add(new MathVector(this.getPoints().get(i),
                                            this.getPoints().get(i + 1)));
-            } else {
-                vectors.add(new MathVector(this.getPoints().get(i),
-                                           this.getPoints().get(0)));
-            }
         }
+        vectors.add(new MathVector(this.getPoints().get(this.getNumberOfPoints() - 1),
+                                   this.getPoints().get(0)));
         return vectors;
     }
 }
