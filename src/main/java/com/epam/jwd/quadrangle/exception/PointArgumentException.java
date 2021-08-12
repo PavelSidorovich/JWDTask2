d5a7d39d57368bfd0aa62aa71a3d054e47e58f20
Pavel Sidorovich <pavsid2001@mail.ru>
2021-08-12 13:51:27 +0300

    remove unnecessary things

--- src/main/java/com/epam/jwd/quadrangle/exception/PointArgumentException.java
+++ src/main/java/com/epam/jwd/quadrangle/exception/PointArgumentException.java
@@ -1,22 +1,8 @@
 package com.epam.jwd.quadrangle.exception;
 
-import com.epam.jwd.quadrangle.model.Point;
-
-import java.util.List;
-
 public class PointArgumentException extends IllegalArgumentException {
-    private static final String ILLEGAL_ARGUMENT_MSG = "%s cannot be build from coordinates: %s";
 
-    private final List<String> coordinates;
-
-    public PointArgumentException(String s, List<String> coordinates) {
+    public PointArgumentException(String s) {
         super(s);
-        this.coordinates = coordinates;
-    }
-
-    @Override
-    public String getMessage() {
-        String message = String.format(ILLEGAL_ARGUMENT_MSG, Point.class.getSimpleName(), coordinates);
-        return super.getMessage() + message;
     }
 }
