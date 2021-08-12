c51b82955146f4c48b5eaabe48ab5070c5beb627
Pavel Sidorovich <pavsid2001@mail.ru>
2021-08-12 13:53:46 +0300

    add validator support to enum

--- src/main/java/com/epam/jwd/quadrangle/model/FigureType.java
+++ src/main/java/com/epam/jwd/quadrangle/model/FigureType.java
@@ -1,19 +1,29 @@
 package com.epam.jwd.quadrangle.model;
 
+import com.epam.jwd.quadrangle.validation.PointValidator;
+import com.epam.jwd.quadrangle.validation.QuadrangleBuildValidator;
+import com.epam.jwd.quadrangle.validation.Validator;
+
 public enum FigureType {
-    POINT(1),
-    LINE(2),
-    TRIANGLE(3),
-    QUADRANGLE(4),
+    POINT(1, new PointValidator()),
+    LINE(2, null),
+    TRIANGLE(3, null),
+    QUADRANGLE(4, new QuadrangleBuildValidator()),
     ;
 
     private final int numberOfPoints;
+    private final Validator validator;
 
-    FigureType(int numberOfPoints) {
+    FigureType(int numberOfPoints, Validator validator) {
         this.numberOfPoints = numberOfPoints;
+        this.validator = validator;
     }
 
     public int getNumberOfPoints() {
         return numberOfPoints;
+    }
+
+    public Validator getValidator() {
+        return validator;
     }
 }
