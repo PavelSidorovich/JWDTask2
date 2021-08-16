29edf2ce3227ad17983ffdb849febf79e38195e5
Pavel Sidorovich <pavsid2001@mail.ru>
2021-08-16 16:54:49 +0300

    code cleanup

--- src/test/java/com/epam/jwd/quadrangle/model/QuadrangleFactoryTest.java
+++ src/test/java/com/epam/jwd/quadrangle/model/QuadrangleFactoryTest.java
@@ -10,34 +10,34 @@ import static org.testng.Assert.*;
 
 public class QuadrangleFactoryTest {
 
-    private final QuadrangleFactory QUADRANGLE_FABRIC = QuadrangleFactory.getInstance();
-    private final PointFactory POINT_FABRIC = PointFactory.getInstance();
-    private final LinkedList<Point> POINTS = new LinkedList<>();
+    private final QuadrangleFactory quadrangleFactory = QuadrangleFactory.getInstance();
+    private final PointFactory pointFactory = PointFactory.getInstance();
+    private final LinkedList<Point> points = new LinkedList<>();
 
     @AfterMethod
     public void clearList() {
-        POINTS.clear();
+        points.clear();
     }
 
     @Test
     public void newInstance_shouldReturnQuadrangle_whenCoordinatesAreValid() {
-        POINTS.add(POINT_FABRIC.of(0, 4));
-        POINTS.add(POINT_FABRIC.of(3, 4));
-        POINTS.add(POINT_FABRIC.of(5, 9));
-        POINTS.add(POINT_FABRIC.of(6, 1));
-        Quadrangle quadrangle = QUADRANGLE_FABRIC.of(POINTS);
+        points.add(pointFactory.of(0, 4));
+        points.add(pointFactory.of(3, 4));
+        points.add(pointFactory.of(5, 9));
+        points.add(pointFactory.of(6, 1));
+        Quadrangle quadrangle = quadrangleFactory.of(points);
 
         assertNotNull(quadrangle);
     }
 
     @Test
     public void newInstance_throwFigureException_whenNumberOfCoordinatesIsInvalid() {
-        POINTS.add(POINT_FABRIC.of(0, 4));
-        POINTS.add(POINT_FABRIC.of(3, 4));
-        POINTS.add(POINT_FABRIC.of(5, 9));
+        points.add(pointFactory.of(0, 4));
+        points.add(pointFactory.of(3, 4));
+        points.add(pointFactory.of(5, 9));
 
         try {
-            QUADRANGLE_FABRIC.of(POINTS);
+            quadrangleFactory.of(points);
             fail("should throw FigureBuildException");
         } catch (FigureBuildException figureBuildException) {
             assertNotNull(figureBuildException);
@@ -47,13 +47,13 @@ public class QuadrangleFactoryTest {
 
     @Test
     public void newInstance_throwFigureException_whenLinesAreCrossing() {
-        POINTS.add(POINT_FABRIC.of(0, 0));
-        POINTS.add(POINT_FABRIC.of(1, 0));
-        POINTS.add(POINT_FABRIC.of(2, 0));
-        POINTS.add(POINT_FABRIC.of(4, 9));
+        points.add(pointFactory.of(0, 0));
+        points.add(pointFactory.of(1, 0));
+        points.add(pointFactory.of(2, 0));
+        points.add(pointFactory.of(4, 9));
 
         try {
-            QUADRANGLE_FABRIC.of(POINTS);
+            quadrangleFactory.of(points);
             fail("should throw FigureBuildException");
         } catch (FigureBuildException figureBuildException) {
             assertNotNull(figureBuildException);
