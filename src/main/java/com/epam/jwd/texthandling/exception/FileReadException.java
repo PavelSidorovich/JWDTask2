e4d0dde44b1bf82af9f837b903af619f5a4107d6
Pavel Sidorovich <pavsid2001@mail.ru>
2021-08-30 10:33:38 +0300

    add exceptions

---
+++ src/main/java/com/epam/jwd/texthandling/exception/FileReadException.java
@@ -0,0 +1,7 @@
+package com.epam.jwd.texthandling.exception;
+
+public class FileReadException extends RuntimeException {
+    public FileReadException(Throwable cause) {
+        super(cause);
+    }
+}
