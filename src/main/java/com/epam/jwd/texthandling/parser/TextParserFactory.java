9da65326e5ea2f44aa7a64ace1260956494daef8
Pavel Sidorovich <pavsid2001@mail.ru>
2021-09-01 12:09:03 +0300

    add comments

--- src/main/java/com/epam/jwd/texthandling/parser/TextParserFactory.java
+++ src/main/java/com/epam/jwd/texthandling/parser/TextParserFactory.java
@@ -1,11 +1,6 @@
 package com.epam.jwd.texthandling.parser;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class TextParserFactory {
-
-    private static final Logger LOG = LogManager.getLogger(TextParserFactory.class);
 
     private static TextParserFactory factory;
 
