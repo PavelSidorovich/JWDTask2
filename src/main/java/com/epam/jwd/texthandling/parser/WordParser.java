34e59a3d73e95ea60cc06e9e1ddacfb571abe492
Pavel Sidorovich <pavsid2001@mail.ru>
2021-09-03 22:41:17 +0300

    make getSymbolAfterWord() method return Optional value

--- src/main/java/com/epam/jwd/texthandling/parser/WordParser.java
+++ src/main/java/com/epam/jwd/texthandling/parser/WordParser.java
@@ -3,12 +3,12 @@ package com.epam.jwd.texthandling.parser;
 import com.epam.jwd.texthandling.model.Symbol;
 import com.epam.jwd.texthandling.model.TextComponent;
 import com.epam.jwd.texthandling.model.Word;
-import com.epam.jwd.texthandling.reader.TextFile;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,7 +18,7 @@ public class WordParser extends TextParser {
 
     private static final String WORD_REGEX = "\\s";
     private static final String WORD_IS_WORKING_MSG = String.format("%s is working", WordParser.class);
-    private static final String WORD_WITH_SYMBOLS_REGEX = ".+([,:]?)";
+    private static final String WORD_WITH_SYMBOLS_REGEX = ".+([,:])";
 
     private static WordParser instance;
 
@@ -41,17 +41,18 @@ public class WordParser extends TextParser {
 
         for (String word : words) {
             wordList.add(new Word(word));
-            wordList.add(new Symbol(getSymbolAfterWord(word)));
+            Optional<String> symbolAfterWord = getSymbolAfterWord(word);
+            symbolAfterWord.ifPresent(s -> wordList.add(new Symbol(s)));
         }
         return wordList;
     }
 
-    private String getSymbolAfterWord(String word) {
+    private Optional<String> getSymbolAfterWord(String word) {
         Matcher matcher = Pattern.compile(WORD_WITH_SYMBOLS_REGEX).matcher(word);
 
         if (matcher.find()) {
-            return matcher.group(1); // extract symbol
+            return Optional.ofNullable(matcher.group(1)); // extract symbol
         }
-        return "";
+        return Optional.empty();
     }
 }
