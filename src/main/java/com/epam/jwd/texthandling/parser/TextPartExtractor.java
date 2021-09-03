ba460ec5884b10f30d22a0d400e536b5eaf8c551
Pavel Sidorovich <pavsid2001@mail.ru>
2021-09-03 22:40:26 +0300

    BREAKING CHANGE: edit text extractor (delete linking symbols)

--- src/main/java/com/epam/jwd/texthandling/parser/TextPartExtractor.java
+++ src/main/java/com/epam/jwd/texthandling/parser/TextPartExtractor.java
@@ -6,7 +6,6 @@ import com.epam.jwd.texthandling.model.TextPart;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class TextPartExtractor {
 
@@ -14,12 +13,6 @@ public class TextPartExtractor {
         List<TextComponent> result = new ArrayList<>();
 
         extract(result, text.clone(), type);
-
-        if (type == TextPart.SENTENCE) {
-            result = linkSentencesWithSymbols(result);
-        } else {
-            result = result.stream().filter(part -> part.getLexemeLength() != 0).collect(Collectors.toList());
-        }
 
         return result;
     }
@@ -48,18 +41,5 @@ public class TextPartExtractor {
             // It is needed to include symbols in a result so that sentences remain with their endings
             result.add(text);
         }
-    }
-
-    /**
-     * Method links sentences with symbols on a "word" level. It is needed to make right sort of sentences
-     */
-    private static List<TextComponent> linkSentencesWithSymbols(List<TextComponent> parts) {
-        List<TextComponent> sentences = new ArrayList<>();
-        for (int i = 0; i < parts.size() - 1; i += 2) {
-            TextComponent p = parts.get(i);
-            ((TextComposite) p).addPart(parts.get(i + 1));
-            sentences.add(p);
-        }
-        return sentences;
     }
 }
