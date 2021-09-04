2ced4f83ac7c21946e5da045b76bdbf64b4b3c48
Pavel Sidorovich <pavsid2001@mail.ru>
2021-09-05 00:29:45 +0300

    add exceptions

---
+++ src/main/java/com/epam/jwd/texthandling/exception/InvalidExpressionException.java
@@ -0,0 +1,10 @@
+package com.epam.jwd.texthandling.exception;
+
+public class InvalidExpressionException extends Exception {
+    public InvalidExpressionException() {
+    }
+
+    public InvalidExpressionException(String message) {
+        super(message);
+    }
+}
